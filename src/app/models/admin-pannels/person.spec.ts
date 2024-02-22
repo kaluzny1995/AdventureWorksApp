9b4e4773caaa2ed90fe31d53eaf79cb91db18582
kaluzny1995 <dzh.awaria@gmail.com>
2024-02-22 23:02:23 +0100

    Instructions for admin pannels

--- src/app/models/admin-pannels/person.spec.ts
+++ src/app/models/admin-pannels/person.spec.ts
@@ -1,7 +1,8 @@
+import { EPersonType } from './e-person-type';
 import { Person } from './person';
 
 describe('Person', () => {
   it('should create an instance', () => {
-    expect(new Person()).toBeTruthy();
+    expect(new Person(0, EPersonType.GC, '0', null, 'Name', null, "LastName", null, 0, null, null, "uuid", new Date())).toBeTruthy();
   });
 });
